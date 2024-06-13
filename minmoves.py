778370f0998e06ee2d97422f139b53ebe2b1de1d
Mogul <mogul27442@gmail.com>
2024-06-13 15:16:53 +0100

    Update minmoves.py

--- minmoves.py
+++ minmoves.py
@@ -1,11 +1,10 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
 
-        distance_counter = 0
         students.sort()
         seats.sort()
-        new_list = zip(students,seats)
 
         distances = [abs(student-seat) for student, seat in zip(students, seats)]
 
         return sum(distances)
+            
